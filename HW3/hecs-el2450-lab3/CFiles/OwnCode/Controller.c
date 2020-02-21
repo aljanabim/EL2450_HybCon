3cdba05ee76d537b5dc425ae5f494f9aed3173b0
Mustafa Al-Janabi <ma970101@yahoo.se>
2020-02-21 19:40:53 +0100

    added control switching logic

--- HW3/hecs-el2450-lab3/CFiles/OwnCode/Controller.c
+++ HW3/hecs-el2450-lab3/CFiles/OwnCode/Controller.c
@@ -1,4 +1,58 @@
-w = k_psi * (theta_goal - theta);
+#include <math.h>
+theta_goal = atan2(ygoal, xgoal) * 180 / PI;
+controller = 10;
+angle_threshold = 1;
+w = 0;
+distance_threshold = 5;
 
-right = vel - w / 2;
-left = vel + w / 2;
+if (abs(theta_goal - theta) > angle_threshold)
+{
+    controller = 0;
+}
+else if (sqrt(pow(xg - x, 2) + pow(yg - y, 2)) > distance_threshold)
+{
+    controller = 1;
+}
+
+switch (controller)
+{
+case 0: // rotational conttroller
+    k_psi = 5;
+    k_w = 5;
+
+    xgoal = (xg - x) / 100;
+    ygoal = yg - y;
+    // w = k_psi * (theta_goal - theta);
+
+    xdelta = x0 - x;
+    ydelta = y0 - y;
+    d0 = cos(theta * PI / 180) * xdelta + sin(theta * PI / 180) * ydelta;
+    vel = k_w * d0;
+
+case 1:
+
+    break;
+
+default:
+    vel = 0;
+    w = 0;
+    break;
+}
+
+left = vel - w / 2;
+right = vel + w / 2;
+
+Serial.print("Theta error: ");
+Serial.print(theta_goal - theta);
+Serial.print(" | xdelta: ");
+Serial.print(xdelta);
+Serial.print(" | ydelta: ");
+Serial.print(ydelta);
+Serial.print(" | d0: ");
+Serial.print(d0);
+Serial.print(" | x ");
+Serial.print(x);
+Serial.print(" | x0 ");
+Serial.print(x0);
+Serial.print(" | xg ");
+Serial.print(xg);
---
+++ HW3/logs/_log_plotter.py
@@ -0,0 +1,46 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import csv
+import sys
+np.set_printoptions(threshold=sys.maxsize)
+plt.style.use('ggplot')
+
+
+def get_data(filename):
+    with open(filename) as csv_file:
+        raw = np.array(list(csv.reader(csv_file, delimiter=';'))
+                       )[1:].astype("float64")
+        time = raw[:, 0]-raw[0, 0]
+        x = raw[:, 1]
+        y = raw[:, 2]
+        theta = raw[:, 3]
+    return time, x, y, theta
+
+
+def task6():
+    time8, x8, y8, theta8 = get_data('pos_task_6_k_8.csv')
+    time5, x5, y5, theta5 = get_data('pos_task_6_k_5.csv')
+    min_dim = np.minimum(time8.shape[0], time5.shape[0])-1
+
+    plt.plot(time8[:min_dim], theta8[:min_dim], label=r"$K_{\Psi}=8$")
+    plt.plot(time5[:min_dim], theta5[:min_dim], label=r"$K_{\Psi}=5$")
+    plt.xlabel("Time [ms]")
+    plt.ylabel(r"$\Theta$")
+    plt.legend()
+    plt.savefig('plots/task6.pdf')
+    # plt.show()
+
+
+def task8():
+    time8, x8, y8, theta8 = get_data('pos_task_8.csv')
+    goal = np.array([0.7269, 0.5912])
+    error_x = x8-goal[0]
+    error_y = y8-goal[1]
+    start = np.array([0, 0])
+    dist = start-goal
+    plt.plot(time8, np.sqrt((error_x)**2+(error_y)**2))
+    plt.show()
+
+
+if __name__ == "__main__":
+    task8()

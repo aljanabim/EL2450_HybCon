47f83ad5bddc5d4fe0a88f25ec92d379041159fb
Mustafa Al-Janabi <ma970101@yahoo.se>
2020-02-21 15:23:03 +0100

    rotation controller

--- HW3/hecs-el2450-lab3/CFiles/OwnCode/Controller.c
+++ HW3/hecs-el2450-lab3/CFiles/OwnCode/Controller.c
@@ -1,2 +1,4 @@
-right = 100;
-left = -100;
+w = k_psi * (theta_goal - theta);
+
+right = vel - w / 2;
+left = vel + w / 2;
---
+++ HW3/hecs-el2450-lab3/CFiles/OwnCode/OwnVariables.c
@@ -0,0 +1,6 @@
+// int xgoal = xg - x;
+// int ygoal = yg - y;
+double theta_goal = atan2(y, x) * 180 / PI;
+int k_psi = 10;
+int vel = 0;
+int w = 0;

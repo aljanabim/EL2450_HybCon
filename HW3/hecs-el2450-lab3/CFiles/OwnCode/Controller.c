70154ec2b5b93d55963f9106a5daa6aa346360b9
Mustafa Al-Janabi <ma970101@yahoo.se>
2020-02-23 16:52:32 +0100

    controller full

--- HW3/hecs-el2450-lab3/CFiles/OwnCode/Controller.c
+++ HW3/hecs-el2450-lab3/CFiles/OwnCode/Controller.c
@@ -1,12 +1,11 @@
 #include <math.h>
 controller = 10;
 distance_threshold = 5;
+angle_threshold = 1;
 theta_goal = atan2(yg, xg) * 180 / PI;
 p = 2;
-vel = 0;
-w = 0;
 
-if (abs(theta_goal - theta) > angle_threshold)
+if (abs(theta_goal - theta) > 20)
 {
     controller = 0;
 }
@@ -14,9 +13,7 @@ else if (sqrt(pow(xg - x, 2) + pow(yg - y, 2)) > distance_threshold)
 {
     controller = 1;
 }
-
-angle_threshold = 1;
-
+controller = 1;
 switch (controller)
 {
 case 0: // rotational controller
@@ -37,20 +34,28 @@ case 0: // rotational controller
 case 1: // Go-to-goal controller
     // translation
     k_w = 5;
-    xdelta = xgoal - x;
-    ydelta = ygoal - y;
+    xdelta = xg - x;
+    ydelta = yg - y;
     d0 = cos(theta_goal * PI / 180) * xdelta + sin(theta_goal * PI / 180) * ydelta;
     vel = k_w * d0;
 
     //Rotation
-    k_g2g = 5;
-    xdelta = (x + p * cos(theta) - x0) * sin(theta_goal);
-    ydelta = (y + p * sin(theta) - y0) * cos(theta_goal);
+    p = sqrt(pow(xg - x, 2) + pow(yg - y, 2));
+    if (20 < p)
+    {
+        k_g2g = 20;
+    }
+    else
+    {
+        k_g2g = p;
+    }
+
+    Serial.print(p);
+    Serial.print(" k ");
+    Serial.print(k_g2g);
+    xdelta = (x + p * cos(theta * PI / 180) - x0) * sin(theta_goal * PI / 180);
+    ydelta = (y + p * sin(theta * PI / 180) - y0) * cos(theta_goal * PI / 180);
     w = k_g2g * (xdelta - ydelta);
-    xdelta = xg - x;
-    ydelta = yg - y;
-    d0 = cos(theta_goal * PI / 180) * xdelta + sin(theta_goal * PI / 180) * ydelta;
-    w = k_g2g * d0;
 
     break;
 
@@ -64,29 +69,31 @@ default:
 left = vel - w / 2;
 right = vel + w / 2;
 
-Serial.print(" | w: ");
-Serial.print(w);
-Serial.print(" | vel: ");
-Serial.print(vel);
-Serial.print(" | left: ");
-Serial.print(left);
-Serial.print(" | right: ");
-Serial.print(right);
+Serial.print(" Controller ");
+Serial.print(controller);
+// Serial.print(" | w: ");
+// Serial.print(w);
+// Serial.print(" | vel: ");
+// Serial.print(vel);
+// Serial.print(" | left: ");
+// Serial.print(left);
+// Serial.print(" | right: ");
+// Serial.print(right);
 
-Serial.print("Theta error: ");
-Serial.print(theta_goal - theta);
-Serial.print(" | xdelta: ");
-Serial.print(xdelta);
-Serial.print(" | ydelta: ");
-Serial.print(ydelta);
-Serial.print(" | d0: ");
-Serial.print(d0);
-Serial.print(" | x ");
-Serial.print(x);
-Serial.print(" | x0 ");
-Serial.print(x0);
-Serial.print(" | xg ");
-Serial.print(xg);
+// Serial.print("Theta error: ");
+// Serial.print(theta_goal - theta);
+// Serial.print(" | xdelta: ");
+// Serial.print(xdelta);
+// Serial.print(" | ydelta: ");
+// Serial.print(ydelta);
+// Serial.print(" | d0: ");
+// Serial.print(d0);
+// Serial.print(" | x ");
+// Serial.print(x);
+// Serial.print(" | x0 ");
+// Serial.print(x0);
+// Serial.print(" | xg ");
+// Serial.print(xg);
 
 // 0;0;-1.25;1.25
 // -1.25;1.25;-0.75;1.25
